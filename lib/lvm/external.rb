sous-chefs/chef-ruby-lvm

commit 9e73c2832e4ab5af89353af6a57e5838e3ff566b

Freeze more contants to reduce memory usage (#9)
Signed-off-by: Dan Webb <dan.webb@damacus.io>

lib/lvm/external.rb
@@ -9,9 +9,7 @@ module LVM
       output = []
       error = nil
       stat = Open4.popen4(cmd) do |pid, stdin, stdout, stderr|
-        while line = stdout.gets
-          output << line
-        end
+        output << line while line == stdout.gets
         error = stderr.read.strip
       end
       if stat.exited?
@@ -25,9 +23,9 @@ module LVM
       end
 
       if block_given?
-        return output.each { |l| yield l }
+        output.each { |l| yield l }
       else
-        return output.join
+        output.join
       end
     end
     module_function :cmd

lib/lvm/version.rb
@@ -1,3 +1,3 @@
 module LVM
-  VERSION = "0.4.0"
+  VERSION = "0.4.0".freeze
 end

Rakefile
@@ -1,6 +1,6 @@
 require "bundler/gem_tasks"
 
-task :default => :style
+task default: :style
 
 require "chefstyle"
 require "rubocop/rake_task"

chef-ruby-lvm.gemspec
@@ -1,4 +1,4 @@
-$:.unshift(File.expand_path('lib', __dir__))
+$:.unshift(File.expand_path("lib", __dir__))
 require "lvm/version"
 
 Gem::Specification.new do |gem|

examples/additional_arguments.rb
@@ -7,7 +7,7 @@ $: << __dir__ + "/../lib"
 require "lvm"
 
 # List out volume groups with the --ignoreskippedcluster argument appended to the base argument list
-lvm = LVM::LVM.new(:command => "/usr/bin/sudo /sbin/lvm", :additional_arguments => "--ignoreskippedcluster")
+lvm = LVM::LVM.new(command: "/usr/bin/sudo /sbin/lvm", additional_arguments: "--ignoreskippedcluster")
 
 volume_groups = lvm.volume_groups
-puts volume_groups.map { |vg| vg.name }
+puts volume_groups.map(&:name)

examples/create_snapshot.rb
@@ -10,7 +10,7 @@ require "lvm"
 vol = "sys.vg/tmp.lv"
 snap = "demo_snap"
 
-lvm = LVM::LVM.new(:command => "/usr/bin/sudo /sbin/lvm")
+lvm = LVM::LVM.new(command: "/usr/bin/sudo /sbin/lvm")
 
 if lvm.logical_volumes[snap]
   puts "#{snap} exists! Remove it"
@@ -20,9 +20,9 @@ end
 lvm.raw("lvcreate --snapshot --size 10M --name #{snap} #{vol}")
 lv = lvm.logical_volumes[snap]
 
-out = <<msg
+out = <<MSG
 snapshot of #{vol}, #{snap}, created
 - uuid: #{lv.uuid}
 - major,minor: #{lv.kernel_major},#{lv.kernel_minor}
-msg
+MSG
 puts out

examples/error_handling.rb
@@ -6,7 +6,7 @@ $: << __dir__ + "/../lib"
 
 require "lvm"
 
-lvm = LVM::LVM.new(:command => "/usr/bin/sudo /sbin/lvm")
+lvm = LVM::LVM.new(command: "/usr/bin/sudo /sbin/lvm")
 
 begin
   lvm.raw("--blah blah")

examples/show_lvm_config.rb
@@ -7,7 +7,7 @@ $: << __dir__ + "/../lib"
 
 require "lvm"
 
-LVM::LVM.new({ :command => "/usr/bin/sudo /sbin/lvm" }) do |lvm|
+LVM::LVM.new({ command: "/usr/bin/sudo /sbin/lvm" }) do |lvm|
   puts "lvm version: #{lvm.version}\n"
 
   puts "\nlogical view"

commit 8b7233d78d801438e63c525455134db4e4080ae5

Fix undefined line variable error in LVM::External
This is a regression that was introduced in commit
9e73c2832e4ab5af89353af6a57e5838e3ff566b. The change is a simple fix
to ensure the line variable is defined at the time it is accessed.

Fixes #10

Signed-off-by: Matthew Newell <matthew.newell@cerner.com>

lib/lvm/external.rb
@@ -9,7 +9,9 @@ module LVM
       output = []
       error = nil
       stat = Open4.popen4(cmd) do |pid, stdin, stdout, stderr|
-        output << line while line == stdout.gets
+        while (line = stdout.gets)
+          output << line
+        end
         error = stderr.read.strip
       end
       if stat.exited?

lib/lvm/version.rb
@@ -1,3 +1,3 @@
 module LVM
-  VERSION = "0.4.1".freeze
+  VERSION = "0.4.2".freeze
 end